b43668cad3230cd681b844041b26dcc90ded0b6a
Weiyan Hu <43009969+hu611@users.noreply.github.com>
2023-04-12 20:58:13 +0800

    restructured view for merchant display. Changed from listview to gridview.

---
+++ Android_Code/app/src/main/java/cs/hku/hk/android_code/CustomAdapter.java
@@ -0,0 +1,43 @@
+package cs.hku.hk.android_code;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class CustomAdapter extends BaseAdapter {
+    Context context;
+    int[] imgIds;
+    String[] txtIds;
+    LayoutInflater inflter;
+    public CustomAdapter(Context applicationContext, int[] imgIds, String[] txtIds) {
+        this.context = applicationContext;
+        this.imgIds = imgIds;
+        this.txtIds = txtIds;
+        inflter = (LayoutInflater.from(applicationContext));
+    }
+    @Override
+    public int getCount() {
+        return imgIds.length;
+    }
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        view = inflter.inflate(R.layout.activity_grid_view, null);
+        ImageView imageView = (ImageView) view.findViewById(R.id.merchant_img);
+        imageView.setImageResource(this.imgIds[i]);
+        TextView textView = (TextView) view.findViewById(R.id.merchant_txt);
+        textView.setText(this.txtIds[i]);
+        return view;
+    }
+}
--- Android_Code/app/src/main/java/cs/hku/hk/android_code/ListItemActivity.java
+++ Android_Code/app/src/main/java/cs/hku/hk/android_code/ListItemActivity.java
@@ -7,27 +7,46 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.app.Activity;
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
 import android.widget.ListView;
 
 public class ListItemActivity extends AppCompatActivity {
+    int[] imgid = {
+            R.drawable.apple,
+            R.drawable.apple,
+            R.drawable.apple,
+            R.drawable.apple,
+            R.drawable.apple,
+            R.drawable.apple,
+            R.drawable.apple
+    };
+
+    String[] textid = {
+            "apple",
+            "banana",
+            "carrot",
+            "apple",
+            "banana",
+            "carrot",
+            "apple"
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_item);
-        // create a new instance of your fragment
-        Fragment fragment = new ListItemFragment();
+        GridView simpleGrid = (GridView) findViewById(R.id.simpleGridView); // init GridView
+        CustomAdapter customAdapter = new CustomAdapter(getApplicationContext(), imgid, textid);
+        simpleGrid.setAdapter(customAdapter);
+        simpleGrid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // set an Intent to Another Activity
 
-        // get the FragmentManager
-        FragmentManager fragmentManager = getSupportFragmentManager();
-
-        // begin a new FragmentTransaction
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-
-        // add the fragment to the container
-        fragmentTransaction.replace(R.id.fragment_container, fragment);
-
-        // commit the transaction
-        fragmentTransaction.commit();
+            }
+        });
     }
 }
